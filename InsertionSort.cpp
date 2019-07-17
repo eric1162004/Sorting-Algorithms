8735fea44ba54537f91adc2fad374d0a7bfe983b
eric1162004 <49351150+eric1162004@users.noreply.github.com>
2019-07-17 13:40:42 -0700

    Add files via upload

---
+++ InsertionSort.cpp
@@ -0,0 +1,33 @@
+//Insertion sort
+
+#include <iostream>
+using namespace std;
+
+void InsertionSort(int a[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int value = a[i];
+        int hole = i;
+
+        while (value < a[hole - 1] && hole > 0)
+        {
+            a[hole] = a[hole - 1];
+            hole--;
+        }
+        a[hole] = value;
+    }
+}
+void printArray(int A[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << A[i] << " ";
+}
+
+int main()
+{
+    int A[] = {9, 3, 6, 5, 2, 1};
+    int n = 6;
+    InsertionSort(A, n);
+    printArray(A, n);
+}
