e875fda652f97d0b1a6ccd7e8b43e80e96ffefc1
eric1162004 <49351150+eric1162004@users.noreply.github.com>
2019-07-17 18:21:57 -0700

    Add files via upload

---
+++ QuickSort.cpp
@@ -0,0 +1,46 @@
+//Quick sort
+
+#include <iostream>
+using namespace std;
+
+int Partition(int *a, int start, int end)
+{
+    int pivot = a[end];
+    int pIndex = start;
+    for (int i = start; i < end; i++)
+    {
+        if (a[i] <= pivot)
+        {
+            swap(a[i], a[pIndex]);
+            pIndex++;
+        }
+    }
+    swap(a[pIndex], a[end]);
+
+    return pIndex;
+}
+
+void QuickSort(int *a, int start, int end)
+{
+    if (start < end)
+    {
+        int pIndex = Partition(a, start, end);
+        QuickSort(a, start, pIndex - 1);
+        QuickSort(a, pIndex + 1, end);
+    }
+}
+
+void printArray(int *A, int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << A[i] << " ";
+}
+
+int main()
+{
+    int A[] = {4, 3, 6, 9, 2, 1};
+    int n = sizeof(A) / sizeof(A[0]);
+    //QuickSort(Array, start index, end index)
+    QuickSort(A, 0, n - 1);
+    printArray(A, n);
+}
